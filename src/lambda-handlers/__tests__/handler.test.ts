b006a93a33b5453c02ebe9c67b3da54ffb68f76e
TimoLaasonen <timo.laasonen@elenium.fi>
2023-01-15 20:32:47 +0200

    added more lambda functions

--- src/lambda-handlers/__tests__/handler.test.ts
+++ src/lambda-handlers/__tests__/handler.test.ts
@@ -1,12 +1,18 @@
-import { handler } from '../app';
+import { fetchAll } from '../app';
+import {APIGatewayProxyEvent, Context} from "aws-lambda";
 const event: any = {
     body: JSON.stringify({}),
     headers: {}
 };
 
 describe('Lambda handler test', () => {
-    test('This is the proof of concept that the test works.', async () => {
-        const res = await handler(event);
+    test('Should fetch all items', async () => {
+        const dummyProxyEvent: Partial<APIGatewayProxyEvent> = {
+            headers: { Authorization: "dummyToken" },
+            body: JSON.stringify({}),
+        };
+
+        const res = await fetchAll(event);
         expect(res.statusCode).toBe(200);
     });
 });
--- src/lambda-handlers/app.ts
+++ src/lambda-handlers/app.ts
@@ -1,9 +1,64 @@
-import { APIGatewayEvent } from 'aws-lambda';
+import { APIGatewayEvent, APIGatewayProxyResult, Context } from 'aws-lambda';
 
-export const handler = async (event: APIGatewayEvent) => {
-    console.info('incoming event is', JSON.stringify(event));
+interface IUser {
+    id: string;
+    name: string;
+}
+
+const data: IUser[] = [
+    {
+        id: 'fa5ebb40-d440-430a-93b3-b0a07e5bd63a',
+        name: 'User 1'
+    },
+    {
+        id: '06964cfb-1212-4fd7-a80e-d825300f9226',
+        name: 'User 2'
+    }
+];
+
+export const fetchAll = async (
+    event: APIGatewayEvent
+): Promise<APIGatewayProxyResult> => {
+    console.log(`Event: ${JSON.stringify(event, null, 2)}`);
+
     return {
         statusCode: 200,
-        body: JSON.stringify({ message: 'Request was successful. Yeah' })
+        body: JSON.stringify(data)
+    };
+};
+
+export const findById = async (
+    event: APIGatewayEvent,
+    context: Context
+): Promise<APIGatewayProxyResult> => {
+    console.log(`Event: ${JSON.stringify(event, null, 2)}`);
+    console.log(`Context: ${JSON.stringify(context, null, 2)}`);
+
+    const id = event.pathParameters?.id;
+    const user: IUser | undefined = data.find((user) => user.id === id);
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify(user)
+    };
+};
+
+export const addNew = async (
+    event: APIGatewayEvent,
+    context: Context
+): Promise<APIGatewayProxyResult> => {
+    console.log(`Event: ${JSON.stringify(event, null, 2)}`);
+    console.log(`Context: ${JSON.stringify(context, null, 2)}`);
+
+    if (!event.body) {
+        throw new Error('Empty content not allowed');
+    }
+
+    const user: IUser = JSON.parse(event.body);
+    data.push(user);
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify(user)
     };
 };
